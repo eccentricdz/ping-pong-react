1be015bf265f3eb9ad9b5e934eee565dabedb11c
Rahul Agarwal <simplyrahul93@gmail.com>
2020-07-04 15:50:03 +0530

    Updates

---
+++ precache-manifest.93b3293b8614c0caa06fd368ef029934.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2643a08b8588e5d4a163bb497dcd43f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0735097cd939a9a75476",
+    "url": "/static/css/main.44f3d670.chunk.css"
+  },
+  {
+    "revision": "c19859e94eceb0b39a58",
+    "url": "/static/js/2.de0794f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.de0794f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0735097cd939a9a75476",
+    "url": "/static/js/main.a88ec86a.chunk.js"
+  },
+  {
+    "revision": "4ad8b91e55a579046896",
+    "url": "/static/js/runtime-main.cdb37037.js"
+  },
+  {
+    "revision": "6d6b5f51e552a050d2357c0cb91a400c",
+    "url": "/static/media/kongtext.6d6b5f51.ttf"
+  }
+]);
